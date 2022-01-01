6f471dfc16d9dba858cb3139741c3b07a823d850
Eromtap <66063750+Eromtap@users.noreply.github.com>
2022-01-01 11:37:37 -0500

    Update main.py

--- Seeds-ML/main.py
+++ Seeds-ML/main.py
@@ -4,6 +4,19 @@
 
 -This will be the main script that contains the UI and calls other modules
 
+-May put import data func into this script so we arent utilizing multiple times
+
+-Need to set training attempt limit. Maybe we let user define like num in millions attempts
+
+-If desired accuracy not achieved after num of attempts, print something like
+oops, i failed, best i could get is (accuracy). Would you like to try again, or
+Save the best values I found.
+
+-Weights and nodes need to be saved to a file (possibly csv) for use with the testing and predicting
+modules
+
+-Also, if we really want this to be truly User Friendly, we need to make this a 
+gui application
 '''
 
 
